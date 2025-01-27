84671ead6e1d4c54d45f518352e6050312997738
Muhammad Ghazi <mghazidev@gmail.com>
2025-01-28 00:36:36 +0500

    crud added for ground owners

--- backend/src/routes/groundOwnerRoutes.ts
+++ backend/src/routes/groundOwnerRoutes.ts
@@ -2,11 +2,19 @@ import { Router } from "express";
 import {
   registerGroundOwner,
   getGroundOwnerList,
+  deleteAllGroundOwners,
+  deleteGroundOwnerById,
+  getGroundOwnerById,
+  updateGroundOwnerById,
 } from "../controllers/groundOwnerController";
 
 const groundOwnerRoutes = Router();
 
 groundOwnerRoutes.post("/register", registerGroundOwner);
 groundOwnerRoutes.get("/get-owners", getGroundOwnerList);
+groundOwnerRoutes.delete("/delete-all", deleteAllGroundOwners);
+groundOwnerRoutes.delete("/:id", deleteGroundOwnerById);
+groundOwnerRoutes.get("/:id", getGroundOwnerById);
+groundOwnerRoutes.put("/:id", updateGroundOwnerById);
 
 export default groundOwnerRoutes;
