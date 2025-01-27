d0f7ea7c44644e1ac5127780b4dcdce583f9d5b1
Muhammad Ghazi <mghazidev@gmail.com>
2025-01-28 00:31:50 +0500

    crud added for ground owners

--- backend/dist/controllers/groundOwnerController.js
+++ backend/dist/controllers/groundOwnerController.js
@@ -12,10 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getGroundOwnerList = exports.registerGroundOwner = void 0;
+exports.updateGroundOwnerById = exports.getGroundOwnerById = exports.deleteGroundOwnerById = exports.deleteAllGroundOwners = exports.getGroundOwnerList = exports.registerGroundOwner = void 0;
 const responseHandler_1 = require("../utils/responseHandler");
 const multerUploader_1 = require("../utils/multerUploader");
 const dao_1 = __importDefault(require("../services/dao"));
+const joi_1 = __importDefault(require("joi"));
 const registerGroundOwner = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const uploadResult = yield (0, multerUploader_1.uploadFiles)(req, res, [
@@ -64,3 +65,75 @@ const getGroundOwnerList = (_req, res) => __awaiter(void 0, void 0, void 0, func
     }
 });
 exports.getGroundOwnerList = getGroundOwnerList;
+const deleteAllGroundOwners = (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield dao_1.default.groundOwnerDAOService.deleteAll();
+        return (0, responseHandler_1.sendResponse)(res, 200, "All ground owners deleted successfully");
+    }
+    catch (error) {
+        console.error(error);
+        return (0, responseHandler_1.sendError)(res, 500, "Server Error", error.message);
+    }
+});
+exports.deleteAllGroundOwners = deleteAllGroundOwners;
+const deleteGroundOwnerById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const groundOwner = yield dao_1.default.groundOwnerDAOService.findById(id);
+        if (!groundOwner) {
+            return (0, responseHandler_1.sendError)(res, 404, "Ground owner not found");
+        }
+        yield dao_1.default.groundOwnerDAOService.deleteById(id);
+        return (0, responseHandler_1.sendResponse)(res, 200, "Ground owner deleted successfully");
+    }
+    catch (error) {
+        console.error(error);
+        return (0, responseHandler_1.sendError)(res, 500, "Server Error", error.message);
+    }
+});
+exports.deleteGroundOwnerById = deleteGroundOwnerById;
+const getGroundOwnerById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const groundOwner = yield dao_1.default.groundOwnerDAOService.findById(id);
+        if (!groundOwner) {
+            return (0, responseHandler_1.sendError)(res, 404, "Ground owner not found");
+        }
+        return (0, responseHandler_1.sendResponse)(res, 200, "Ground owner fetched successfully", groundOwner);
+    }
+    catch (error) {
+        console.error(error);
+        return (0, responseHandler_1.sendError)(res, 500, "Server Error", error.message);
+    }
+});
+exports.getGroundOwnerById = getGroundOwnerById;
+const updateGroundOwnerById = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const { error, value } = joi_1.default.object({
+            fullname: joi_1.default.string().optional().label("Full Name"),
+            contactNo: joi_1.default.string().optional().label("Contact No"),
+            cnicFrontUrl: joi_1.default.string().optional().label("CNIC Front URL"),
+            cnicBackUrl: joi_1.default.string().optional().label("CNIC Back URL"),
+            groundLocation: joi_1.default.string().optional().label("Ground Location"),
+            paymentMethod: joi_1.default.string().optional().label("Payment Method"),
+        }).validate(req.body);
+        if (error) {
+            return (0, responseHandler_1.sendError)(res, 400, "Validation Error", error.details[0].message);
+        }
+        const existingGroundOwner = yield dao_1.default.groundOwnerDAOService.findById(id);
+        if (!existingGroundOwner) {
+            return (0, responseHandler_1.sendError)(res, 404, "Ground owner not found");
+        }
+        const updatedGroundOwner = yield dao_1.default.groundOwnerDAOService.updateByAny({ _id: id }, value);
+        if (!updatedGroundOwner) {
+            return (0, responseHandler_1.sendError)(res, 404, "Ground owner not found");
+        }
+        return (0, responseHandler_1.sendResponse)(res, 200, "Ground owner updated successfully", updatedGroundOwner);
+    }
+    catch (error) {
+        console.error(error);
+        return (0, responseHandler_1.sendError)(res, 500, "Server Error", error.message);
+    }
+});
+exports.updateGroundOwnerById = updateGroundOwnerById;
--- backend/src/controllers/groundOwnerController.ts
+++ backend/src/controllers/groundOwnerController.ts
@@ -4,6 +4,7 @@ import { uploadFiles, UploadResult } from "../utils/multerUploader";
 import { groundOwnerValidationSchema } from "../validations/groundOwnerValidation";
 import datasources from "../services/dao";
 import { IGroundOwner } from "../models/groundOwnerModel";
+import Joi from "joi";
 
 export const registerGroundOwner = async (req: Request, res: Response) => {
   try {
@@ -61,5 +62,87 @@ export const getGroundOwnerList = async (_req: Request, res: Response) => {
   } catch (error: any) {
     console.error(error);
     sendError(res, 500, "Server Error", error.message);
+  }
+};
+
+export const deleteAllGroundOwners = async (_req: Request, res: Response) => {
+  try {
+    await datasources.groundOwnerDAOService.deleteAll();
+    return sendResponse(res, 200, "All ground owners deleted successfully");
+  } catch (error: any) {
+    console.error(error);
+    return sendError(res, 500, "Server Error", error.message);
+  }
+};
+
+export const deleteGroundOwnerById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const groundOwner = await datasources.groundOwnerDAOService.findById(id);
+    if (!groundOwner) {
+      return sendError(res, 404, "Ground owner not found");
+    }
+
+    await datasources.groundOwnerDAOService.deleteById(id);
+    return sendResponse(res, 200, "Ground owner deleted successfully");
+  } catch (error: any) {
+    console.error(error);
+    return sendError(res, 500, "Server Error", error.message);
+  }
+};
+
+export const getGroundOwnerById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const groundOwner = await datasources.groundOwnerDAOService.findById(id);
+    if (!groundOwner) {
+      return sendError(res, 404, "Ground owner not found");
+    }
+    return sendResponse(
+      res,
+      200,
+      "Ground owner fetched successfully",
+      groundOwner
+    );
+  } catch (error: any) {
+    console.error(error);
+    return sendError(res, 500, "Server Error", error.message);
+  }
+};
+
+export const updateGroundOwnerById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { error, value } = Joi.object({
+      fullname: Joi.string().optional().label("Full Name"),
+      contactNo: Joi.string().optional().label("Contact No"),
+      cnicFrontUrl: Joi.string().optional().label("CNIC Front URL"),
+      cnicBackUrl: Joi.string().optional().label("CNIC Back URL"),
+      groundLocation: Joi.string().optional().label("Ground Location"),
+      paymentMethod: Joi.string().optional().label("Payment Method"),
+    }).validate(req.body);
+    if (error) {
+      return sendError(res, 400, "Validation Error", error.details[0].message);
+    }
+    const existingGroundOwner =
+      await datasources.groundOwnerDAOService.findById(id);
+    if (!existingGroundOwner) {
+      return sendError(res, 404, "Ground owner not found");
+    }
+    const updatedGroundOwner =
+      await datasources.groundOwnerDAOService.updateByAny({ _id: id }, value);
+    if (!updatedGroundOwner) {
+      return sendError(res, 404, "Ground owner not found");
+    }
+
+    return sendResponse(
+      res,
+      200,
+      "Ground owner updated successfully",
+      updatedGroundOwner
+    );
+  } catch (error: any) {
+    console.error(error);
+    return sendError(res, 500, "Server Error", error.message);
   }
 };
